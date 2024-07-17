ca0802bbbec72e8a31a1177f67cf31e6f16f46b7
Prasad Kandekar <prasadkandekar555@gmail.com>
2024-07-17 12:01:12 +0530

    initial Setup

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(bodyParser.json({ limit: "50mb", extended: true }));
+app.use(cors());
+app.use(morgan("common"));
+
+// Connect to database
+connectDB().then(() => {
+  console.log('MongoDB connected successfully');
+});
+
+// Health Check
+app.get('/', (req, res) => {
+  res.send('Hello World! Our server is running');
+})
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
