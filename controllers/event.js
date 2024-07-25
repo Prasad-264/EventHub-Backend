0ff3a946ce9c7649f824ac5afb312b387fabcc03
Prasad Kandekar <prasadkandekar555@gmail.com>
2024-07-25 20:37:31 +0530

    modify getEventbyId

--- controllers/event.js
+++ controllers/event.js
@@ -9,7 +9,7 @@ const getEventbyId = async (req, res) => {
       return res.status(404).json({ error: "No event found" });
     }
 
-    res.status(200).json({ event });
+    res.status(200).json( event );
   } catch (error) {
     console.error("Error fetching event", error);
     return res.status(500).json({ error: "No event found" });
