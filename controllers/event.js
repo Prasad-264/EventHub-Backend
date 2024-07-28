a224fb71aa867e9069dfc935143ec8764f83f0f7
Prasad Kandekar <prasadkandekar555@gmail.com>
2024-07-28 19:02:59 +0530

    update getEventbyId

--- controllers/event.js
+++ controllers/event.js
@@ -1,15 +1,17 @@
-const Event = require('../models/Event');
+const Event = require("../models/Event");
 
 const getEventbyId = async (req, res) => {
   try {
     const { eventId } = req.params;
-    const event = await Event.findById(eventId);
-    
+    const event = await Event.findById(eventId).populate([
+      { path: "participants", select: "firstName lastName" },
+    ]);
+
     if (!event) {
       return res.status(404).json({ error: "No event found" });
     }
 
-    res.status(200).json( event );
+    res.status(200).json(event);
   } catch (error) {
     console.error("Error fetching event", error);
     return res.status(500).json({ error: "No event found" });
@@ -18,4 +20,4 @@ const getEventbyId = async (req, res) => {
 
 module.exports = {
   getEventbyId,
-}
+};
